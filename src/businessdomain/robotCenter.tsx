383ef26442a095d9a3045c79ec6c5d4ab74e753b
REN DING <terry.adelaide@gmail.com>
2019-05-07 07:28:19 +1000

    using uuid for array key rather than timestamp plus array index

--- src/businessdomain/robotCenter.tsx
+++ src/businessdomain/robotCenter.tsx
@@ -25,8 +25,8 @@ const reducer = (state:Robot={position:[0,0],faceDirection:'NORTH',reportHistory
 
     switch(action.type){
         case 'PLACE':
-            if(action.position == undefined 
-            || action.faceDirection == undefined
+            if(action.position === undefined 
+            || action.faceDirection === undefined
             || action.position[0] < 0
             || action.position[0] >= mapSize[0]
             || action.position[1] < 0
@@ -35,13 +35,13 @@ const reducer = (state:Robot={position:[0,0],faceDirection:'NORTH',reportHistory
             
             return {...state, ...{position:action.position, faceDirection:action.faceDirection}}
         case 'MOVE':
-            if(state.faceDirection == 'NORTH' && state.position[1] + 1 < mapSize[1])
+            if(state.faceDirection === 'NORTH' && state.position[1] + 1 < mapSize[1])
                 return {...state, ...{position: [state.position[0], state.position[1]+1]}};
-            if(state.faceDirection == 'SOUTH' && state.position[1] - 1 >= 0)
+            if(state.faceDirection === 'SOUTH' && state.position[1] - 1 >= 0)
                 return {...state, ...{position: [state.position[0], state.position[1]-1]}};
-            if(state.faceDirection == 'EAST' && state.position[0] + 1 < mapSize[0])
+            if(state.faceDirection === 'EAST' && state.position[0] + 1 < mapSize[0])
                 return {...state, ...{position: [state.position[0]+1, state.position[1]]}};
-            if(state.faceDirection == 'WEST' && state.position[0] - 1 >= 0)
+            if(state.faceDirection === 'WEST' && state.position[0] - 1 >= 0)
                 return {...state, ...{position: [state.position[0] -1, state.position[1]]}};
             return state;
         case 'LEFT':
@@ -63,9 +63,9 @@ const inputCommandsConverter = (commandsString:string):Array<Action> => {
     return commandsString.split('\n').map(commandString=>{
         if(commandString.startsWith('PLACE')){
             const info = commandString.split(' ')[1];
-            if(info == undefined) return {type:'UNKNOWN'};
+            if(info === undefined) return {type:'UNKNOWN'};
             const infoArray = info.split(',');
-            if(infoArray.length!= 3) return {type:'UNKNOWN'};
+            if(infoArray.length!== 3) return {type:'UNKNOWN'};
             const positionX = parseInt(infoArray[0]);
             const positionY = parseInt(infoArray[1]);
             if(isNaN(positionX) || isNaN(positionY) || !DIRECTION.includes(infoArray[2])) return {type:'UNKNOWN'};
--- src/components/ReportPanel/ReportPanel.tsx
+++ src/components/ReportPanel/ReportPanel.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './styles/ReportPanel.css'
+const uuidv4 = require('uuid/v4');
 
 interface Props {
   results:Array<string>
@@ -15,7 +16,7 @@ export default class ReportPanel extends Component<Props> {
       <div id='report-panel-wrapper' className='report-panel-wrapper'>
         <span className='report-panel-label'>Results:</span>
         <div id='report-panel-results' className='report-panel-results'>
-          {this.props.results.map((value,index)=> (<div key={Date.now()+index}>{value}</div>))}
+          {this.props.results.map((value,index)=> (<div key={uuidv4()}>{value}</div>))}
         </div>
       </div>
     );
